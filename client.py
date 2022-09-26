e7e953af5e724dc061ffbc4faf4327efa5e5ea39
Cameron Liddell <camliddell@outlook.com>
2022-09-26 19:12:20 +0100

    Successful basics of chatroom

--- client.py
+++ client.py
@@ -5,6 +5,47 @@ import threading
 HOST = "127.0.0.1"
 PORT = 1234
 
+# Listens for messages from the server
+def listening(client):
+    
+    while True:
+        message = client.recv(2048).decode("utf-8")
+
+        if message != "":
+            username = message.split("~")[0]
+            contents = message.split("~")[1]
+
+            print(f"{username}: {contents}")
+        else:
+            print("Message is empty")
+
+def sending(client):
+    
+    while True:
+        print("Type your message: ")
+        message = input("")
+
+        if message != "":
+            client.sendall(message.encode("utf-8"))
+
+        else:
+            print("Message empty")
+
+def chat_to_server(client):
+    
+    username = input("Username: ")
+    if username != "":
+        client.sendall(username.encode("utf-8"))
+
+    else:
+        print("Username empty")
+        chat_to_server(client)
+
+    thread = threading.Thread(target=listening, args=(client,))
+    thread.start()
+
+    sending(client)
+
 def main():
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,9 +53,11 @@ def main():
     try:
         client.connect((HOST, PORT))
         print(f"Connected to {HOST}:{PORT}")
+
     except:
         print(f"Unable to connect to {HOST}:{PORT}")
 
+    chat_to_server(client)
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -4,7 +4,45 @@ import threading
 
 HOST = "127.0.0.1"
 PORT = 1234
-LISTENER_LIMIT = 5
+
+connections = []
+
+def listening(client, username):
+    
+    while True:
+        message = client.recv(2048).decode("utf-8")#
+
+        if message != "":
+            final_msg = username + "~" + message
+            broadcast(final_msg)
+
+        else:
+            print(f"Message recieved from {username} is empty")
+        
+def direct_message(client, message):
+
+    client.sendall(message.encode("utf-8"))
+
+def broadcast(message):
+    
+    for user in connections:
+        direct_message(user[1], message)
+
+def handle_client(client):
+    
+    while True:
+        username = client.recv(2048).decode("utf-8")
+        
+        # New user
+        if username != "":
+            connections.append((username, client))
+            break
+
+        else:
+            client.send("Username empty".encode("utf-8"))
+
+    thread = threading.Thread(target=listening, args=(client, username))
+    thread.start()
 
 def main():
 
@@ -13,10 +51,11 @@ def main():
     try:
         server.bind((HOST, PORT))
         print(f"Running on {HOST}:{PORT}")
+
     except socket.error as msg:
         print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
 
-    server.listen(LISTENER_LIMIT)
+    server.listen()
 
     while True:
         client, address = server.accept()
