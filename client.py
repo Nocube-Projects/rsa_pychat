54286e9aaa75844d2e89ac9e018726308b3f0fe0
Cameron Liddell <55388363+CL107@users.noreply.github.com>
2022-09-28 13:03:40 +0100

    Started creating GUI and moving to class based

--- client.py
+++ client.py
@@ -3,77 +3,111 @@ import socket
 import threading
 
 import tkinter as tk
-import tkinter.ttk as ttk
+from tkinter import Tk, ttk
+import sv_ttk
 
-class Window:
+class menu:
     def __init__(self, master):
         
-        pass
+        #server details
+        self.HOST = "127.0.0.1"
+        self.PORT = 1234
 
+        #ttk theme
+        sv_ttk.set_theme("dark")
 
+        #menu gui
+        self.master = master
+        self.master.title("Python Chat App")
+        self.master.geometry("500x500")
+        self.master.config(bg="Black")
+        self.master.resizable(True, True)
 
-HOST = "127.0.0.1"
-PORT = 1234
+    def home_menu(self):
+        #home menu
+        self.frame = ttk.Frame(self.master)
+        #self.home_menu.place(x=0, y=0, width=500, height=500)
 
-# Listens for messages from the server
-def listening(client):
-    
-    while True:
-        message = client.recv(2048).decode("utf-8")
+        #grid setup
+        self.frame.columnconfigure(0, weight=1)
 
-        if message != "":
-            username = message.split("~")[0]
-            contents = message.split("~")[1]
+        self.frame.rowconfigure(0, weight=1)
+        self.frame.rowconfigure(1, weight=1)
+        self.frame.rowconfigure(2, weight=1)
+        self.frame.rowconfigure(3, weight=1)
+        self.frame.rowconfigure(4, weight=1)
+        self.frame.rowconfigure(5, weight=1)
 
-            print(f"{username}: {contents}")
+        #login button
+        login_button = ttk.Button(self.frame, text="Login", command=self.main)
+        login_button.grid(column=0, row=0, padx=0, pady=0)
+
+        #register button
+        register_button = ttk.Button(self.frame, text="Register")
+        register_button.grid(column=0, row=1, padx=0, pady=0)
+
+        #self.frame.place(x=0,y=0)
+        self.frame.pack(fill="both", expand=1)
+
+        # Listens for messages from the server
+    def listening(self):
+        
+        while True:
+            message = self.client.recv(2048).decode("utf-8")
+
+            if message != "":
+                username = message.split("~")[0]
+                contents = message.split("~")[1]
+
+                print(f"{username}: {contents}")
+            else:
+                print("Message is empty")
+
+    def sending(self):
+        
+        while True:
+            print("Type your message: ")
+            message = input("")
+
+            if message != "":
+                self.client.sendall(message.encode("utf-8"))
+
+            else:
+                print("Message empty")
+
+    def chat_to_server(self):
+        
+        username = input("Username: ")
+        if username != "":
+            self.client.sendall(username.encode("utf-8"))
+
         else:
-            print("Message is empty")
+            print("Username empty")
+            self.chat_to_server()
 
-def sending(client):
-    
-    while True:
-        print("Type your message: ")
-        message = input("")
+        thread = threading.Thread(target=self.listening)
+        thread.start()
 
-        if message != "":
-            client.sendall(message.encode("utf-8"))
+        self.sending()
 
-        else:
-            print("Message empty")
+    def main(self):
 
-def chat_to_server(client):
-    
-    username = input("Username: ")
-    if username != "":
-        client.sendall(username.encode("utf-8"))
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    else:
-        print("Username empty")
-        chat_to_server(client)
+        try:
+            self.client.connect((self.HOST, self.PORT))
+            print(f"Connected to {self.HOST}:{self.PORT}")
+            
 
-    thread = threading.Thread(target=listening, args=(client,))
-    thread.start()
+        except:
+            print(f"Unable to connect to {self.HOST}:{self.PORT}")
+            exit()
 
-    sending(client)
+        self.chat_to_server()
 
-def main():
-
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    try:
-        client.connect((HOST, PORT))
-        print(f"Connected to {HOST}:{PORT}")
-
-    except:
-        print(f"Unable to connect to {HOST}:{PORT}")
-
-    chat_to_server(client)
-
-root = tk.Tk()
-root.title("Chat")
-root.geometry("500x500")
-window = Window(root)
-root.mainloop()
 
 if __name__ == "__main__":
-    main()
+    root = tk.Tk()
+    R = menu(root)
+    R.home_menu()
+    root.mainloop()
